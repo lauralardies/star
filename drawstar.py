45a7b01d96a05b62ded85e362c3aa58f7df5240f
lauralardies <lrodrlar@myuax.com>
2021-12-19 12:39:28 +0100

    cambios

--- drawstar.py
+++ drawstar.py
@@ -1,40 +1,70 @@
 import turtle
 
-turtle.colormode(255)
-turtle.getscreen().bgcolor(100,149,237)
-turtle.title("My Star Drawing")
-turtle.shape("turtle")
-turtle.color((205,190,112),(255,236,139))
-turtle.width(1)
+def star():
 
-def star(num):
-
-    size = 300
-    turtle.penup()
-    turtle.goto(-150,0)
-    turtle.pendown()
+    def gcd(a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
     
-    if num % 2 == 0:
-        angle = 360 / num
+    def draw(num, angle, size):
         turtle.begin_fill()
-        for side in range(num):
-            turtle.fd(size)
-            turtle.rt(angle)
+
+        if num == 6:
+            for side in range(3):
+                turtle.fd(size)
+                turtle.rt(angle)
+            turtle.end_fill()
+
+            turtle.penup()
+            turtle.goto(-150, -100)
+            turtle.pendown()
+
+            turtle.begin_fill()
+            for side in range(3):
+                turtle.fd(size)
+                turtle.lt(angle)
+            turtle.end_fill()
+
+        else:     
+            for side in range(num):
+                turtle.fd(size)
+                turtle.rt(angle)
+            turtle.end_fill()
+
+        turtle.done()
+        
+    turtle.colormode(255)
+    turtle.getscreen().bgcolor(100,149,237)
+    turtle.title("My Star Drawing")
+    turtle.shape("turtle")
+    turtle.color((205,190,112),(255,236,139))
+    turtle.width(1)
+
+    while True:
+        num = turtle.textinput("Draw your star!", "How many points does your star have?")
+        if num.isdigit() == True:
+            num = int(num)
+
+            if num <= 4:
+                print("You can't draw a star with a number smaller than 5.")
+
+            else:
+                if num == 6:
+                    angle = 120
+                else:
+                    for i in range(num // 2, 1, -1):
+                        if gcd(num, i) == 1:
+                            angle = 360 / num * i
+                            break
+
+                turtle.penup()
+                turtle.goto(-150,60)
+                turtle.pendown()
+                
+                draw(num, angle, size = 300)
             
-    else:
-        angle = 180 - (180 / num)
-        turtle.begin_fill()
-        for side in range(num):
-            turtle.fd(size)
-            turtle.rt(angle)
+        else:
+            print("This is not a number! Please introduce a valid answer.")
 
-    turtle.end_fill()
-    turtle.done()
-
-while True:
-    num = (input("How many points do you want your star to have? (Please insert a number): "))
-    if num.isdigit() == True:
-        num = int(num)
-        star(num)
-    else:
-        print("This is not a number! Please introduce a valid answer.")
+star()
