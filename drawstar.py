ddca4366d46ae415113455d79e1c136ec2d1c662
lauralardies <lrodrlar@myuax.com>
2021-12-19 18:30:39 +0100

    retoques

--- drawstar.py
+++ drawstar.py
@@ -8,30 +8,22 @@ def star():
         return a
     
     def draw(num, angle, size):
+        turtle.showturtle()
         turtle.begin_fill()
 
         if num == 6:
-            for side in range(3):
-                turtle.fd(size)
+            for side in range(6):
+                turtle.fd(size/3)
+                turtle.rt(-60)
+                turtle.fd(size/3)
                 turtle.rt(angle)
-            turtle.end_fill()
-
-            turtle.penup()
-            turtle.goto(-150, -100)
-            turtle.pendown()
-
-            turtle.begin_fill()
-            for side in range(3):
-                turtle.fd(size)
-                turtle.lt(angle)
-            turtle.end_fill()
 
         else:     
             for side in range(num):
                 turtle.fd(size)
                 turtle.rt(angle)
-            turtle.end_fill()
 
+        turtle.end_fill()
         turtle.done()
         
     turtle.colormode(255)
@@ -39,17 +31,30 @@ def star():
     turtle.title("My Star Drawing")
     turtle.shape("turtle")
     turtle.color((205,190,112),(255,236,139))
-    turtle.width(1)
+    turtle.width(3)
+    turtle.hideturtle()
+    turtle.penup()
 
     while True:
         num = turtle.textinput("Draw your star!", "How many points does your star have?")
         if num.isdigit() == True:
             num = int(num)
-
+            
             if num <= 4:
-                print("You can't draw a star with a number smaller than 5.")
+                turtle.goto(20, 140)
+                turtle.write("You can't draw a star with\na number smaller than 5.", font=("Futura", 15, "bold italic"))
 
             else:
+                turtle.clear()        
+                turtle.colormode(255)
+                turtle.getscreen().bgcolor(100,149,237)
+                turtle.title("My Star Drawing")
+                turtle.shape("turtle")
+                turtle.color((205,190,112),(255,236,139))
+                turtle.width(3)
+                turtle.hideturtle()
+                turtle.penup()
+
                 if num == 6:
                     angle = 120
                 else:
@@ -58,7 +63,6 @@ def star():
                             angle = 360 / num * i
                             break
 
-                turtle.penup()
                 turtle.goto(-150,60)
                 turtle.pendown()
                 
